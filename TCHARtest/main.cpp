7ce8c7c1852f375e0ecbbae206dddc0a21eb2397
Juraj Antas <juraj.antas@gmail.com>
2018-06-06 23:11:33 +0200

    first change

--- TCHARtest/main.cpp
+++ TCHARtest/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
+#include <string>
+
+typedef char16_t TCHAR;
+
+//pretend for a moment that this is portable c++ code
+//that internaly uses TCHAR (16 bit char strings ending with 0)
+//How do I force LLDB in Xcode to display such string in debug view
+//without resorting to see memory of the given variable?
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    //ok, this is displayed
+    char16_t str1[] = u"1234567";
+    //how do I do same trick for this?
+    TCHAR str2[] = u"1234567890";
+    //and this?
+    TCHAR* pointerToStr1 = str1;
+
+    std::cout << "Put breakpoint here and check values of test and pointerToTest\n";
     return 0;
 }
