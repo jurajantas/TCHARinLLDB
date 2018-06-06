0ad1e0d771897e21241fa630dec0fa9da34e7d48
Juraj Antas <juraj.antas@gmail.com>
2018-06-06 22:55:35 +0200

    Initial Commit

---
+++ TCHARtest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TCHARtest
+//
+//  Created by Juraj Antas on 6/6/18.
+//  Copyright © 2018 Juraj Antas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
